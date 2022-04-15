c7f9058f6a7e4c5bc2e882b0dc38880e933a27a5
Navneet Ranjan <mail.navneetranjan@gmail.com>
2022-04-15 16:29:47 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,11 +1,9 @@
+Calculate Radius-
 radius=int(input("ENTER THE RADIUS :"))
 area_circle= 3.14*radius*radius;
 print("AREA OF CIRCLE IS :",area_circle)
 
-f_name=str(input("Enter the file name "))
-if f_name==".py":
-    print("python")
-elif f_name==".c":
-    print("C program")
-else:
-    print("C++")
+Print Extension-
+File_name= input("Enter File_name: ")
+f = File_name.split(".")
+print ("Extension of file is : " + f[-1])
